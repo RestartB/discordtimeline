7b3fdfc18069c6e11f4fea2055fe5a81dfeface1
Restart <79792966+RestartB@users.noreply.github.com>
2024-05-17 19:45:50 +0100

    Add Direct Message disclaimer

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ try:
 except Exception as error:
     print("An error has occured while finding files! Please check the path provided is valid!\nIf you are sure the path is valid, open a GitHub issue and share the following info:")
     print(error)
+    print("\nGitHub Issues Link: https://github.com/restartb/discordtimeline/issues")
     exit()
 
 # Return error if we can't find any channel / message files
@@ -46,35 +47,45 @@ allMessages = []
 
 # --- Main ---
 # Iterate through all channels in package
-print("Reading files.")
-for currentChannelFile in channelFiles:
-    with open(currentChannelFile, "r", errors="ignore") as channelFile:
-        # Load JSON
-        channelData = json.load(channelFile)
+try:
+    print("Reading files.")
+    for currentChannelFile in channelFiles:
+        with open(currentChannelFile, "r", errors="ignore") as channelFile:
+            # Load JSON
+            channelData = json.load(channelFile)
 
-        # Set values
-        try:
-            id = channelData["id"]
-            server = channelData["guild"]["name"]
-            channel = channelData["name"]
+            # Set values
+            try:
+                # Show Direct Message when type is 1
+                if channelData["type"] == 0:
+                    id = channelData["id"]
+                    server = channelData["guild"]["name"]
+                    channel = channelData["name"]
+                    rich = f"{id} - #{channel} in {server}"
+                else:
+                    id = channelData["id"]
+                    rich = f"{id} - Direct Message (IDs: {channelData['recipients']})"
+            except KeyError:
+                id = channelData["id"]
+                rich = f"{id} - No Data"
 
-            rich = f"{id} - #{channel} in {server}"
-        except KeyError:
-            id = channelData["id"]
-            rich = id
+            # Get channel file path
+            head, tail = os.path.split(currentChannelFile)
+            channelPath = os.path.join(head, "messages.json")
+            
+            # Open channel's message file
+            with open(channelPath, "r", errors="ignore") as messageFile:
+                messageData = json.load(messageFile)
 
-        # Get channel file path
-        head, tail = os.path.split(currentChannelFile)
-        channelPath = os.path.join(head, "messages.json")
-        
-        # Open channel's message file
-        with open(channelPath, "r", errors="ignore") as messageFile:
-            messageData = json.load(messageFile)
-
-            # Add message to global message list
-            for message in messageData:
-                data = [id, rich, message['Timestamp'], message['Contents']]
-                allMessages.append(data)
+                # Add message to global message list
+                for message in messageData:
+                    data = [id, rich, message['Timestamp'], message['Contents']]
+                    allMessages.append(data)
+except Exception as error:
+    print("ERROR: Error has occured while reading messages! Try again, or open a GitHub issue and share the following info:")
+    print(error)
+    print("\nGitHub Issues Link: https://github.com/restartb/discordtimeline/issues")
+    exit()
 
 lastMessage = [None]
 richList = []
@@ -83,7 +94,9 @@ richList = []
 print("Sorting lists...")
 allMessages.sort(key=itemgetter(2))
 
-richList.append(f"Restart's Discord Timeline\nYou have {len(allMessages)} messages on Discord\n\n---------------\n")
+richList.append("Discord Message Timeline\n"
+                "Made by @restartb in 2024 - https://github.com/restartb/discordtimeline\n\n"
+                f"You have {len(allMessages)} messages available on Discord")
 
 # Generate rich strings for final .txt file
 print("Generating strings...")
